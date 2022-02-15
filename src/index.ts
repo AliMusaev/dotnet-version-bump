d2a129876c1264c0696e75e190393f44d00b5ccd
Ali Musaev <musaev.ae@modul.tech>
2022-02-15 17:50:31 +0300

    d

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,26 @@ import fs from 'fs';
 try {
     const csproj = core.getInput('csprojFile');
     console.log(`filepath ${csproj}`);
-    const file = fs.readFileSync(csproj).toString();
-    console.log(file);
-    let index = file.indexOf('<Version>');
-    if (index === -1) {
-        throw new Error('Not found version row');   
-    }
-    let version :string = 'v';
-    index += 9; // skip version word
-    while (file[index] !== '<') {
-        version = version + file[index];
-    }
-    core.setOutput('version', version);
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    const file = fs.readFile(csproj, (err, data)=> {
+        if (err) {
+            throw err;
+        }
+        const file = data.toString();
+        console.log(file);
+        let index = file.indexOf('<Version>');
+        if (index === -1) {
+            throw new Error('Not found version row');   
+        }
+        let version :string = 'v';
+        index += 9; // skip version word
+        while (file[index] !== '<') {
+            version = version + file[index];
+        }
+        core.setOutput('version', version);
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+    })
+    
 } catch (error: any) {
     core.setFailed(error.message);
 }
