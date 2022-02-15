f08cffd8c2b3e60700a326846eb56506f3699d1d
Ali Musaev <musaev.ae@modul.tech>
2022-02-15 18:45:57 +0300

    a`s

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,17 @@ const main = async () => {
         console.log(`filepath ${csproj}`);
         const file = await fs.promises.readFile(csproj, 'utf-8');
         console.log(file);
-        let index = file.indexOf('<Version>');
-        if (index === -1) {
+        let index = file.includes('<Version>');
+        if (index === false) {
             throw new Error('Not found version row');   
         }
         console.log('index ' + index)
-        let version :string = 'v';
-        index += 9; // skip version word
-        while (file[index] !== '<') {
-            version = version + file[index];
-        }
-        core.setOutput('version', version);
+        // let version :string = 'v';
+        // index += 9; // skip version word
+        // while (file[index] !== '<') {
+        //     version = version + file[index];
+        // }
+        // core.setOutput('version', version);
         const payload = JSON.stringify(github.context.payload, undefined, 2)
         console.log(`The event payload: ${payload}`);
     } catch (err: any) {
