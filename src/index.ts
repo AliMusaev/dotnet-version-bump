5c814d6f193a29c316cf659d861e668432be666e
Ali Musaev <musaev.ae@modul.tech>
2022-02-16 17:17:04 +0300

    changes

--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,54 @@ import fs from 'fs';
 
 const main = async () => {
     try {
-        const file = await readFile();
-        let index = file.indexOf('<Version>');
-        let version = '';
-        index += 9; // skip version word
-        while (file[index] !== '<') {
-            version = version + file[index];
-            index++
+        const file = await readFile(core.getInput('filename'));
+        let oldVersion = ''
+        let newVersion = '';
+        
+        switch (core.getInput('dotnetVersion')) {
+            case '4':
+                oldVersion = readVersion(file, 'Version(\"', '\"');
+                break;
+            case '5':
+                oldVersion = readVersion(file, '<Version>', '<');
+
         }
-        var newVersion = upVersion(getVersion(version))
-        const newFile = file.replace(version, newVersion);
-        await fs.promises.writeFile(core.getInput('csprojFile'), newFile);
-        console.log(await readFile());
+        newVersion = castArrayVersionToString(upVersion(castStringVersionToArray(oldVersion)));
+        const newFile = file.replace(oldVersion, newVersion);
+        await fs.promises.writeFile(core.getInput('filename'), newFile);
         core.setOutput('version', newVersion);
     } catch (err: any) {
         core.setFailed(err.message)
     }
 }
 
-const readFile  = async () => {
-    const csproj = core.getInput('csprojFile');
-    core.debug(`filepath ${csproj}`);
-    const file = await fs.promises.readFile(csproj, 'utf-8');
-    core.debug(file);
+const updateAssemblyFile = async(filename: string) => {
+    const file = await readFile(filename);
+    
+}
+
+const readVersion = (file: string, substring: string, lastSymbol: string) => {
+    let index: number = file.indexOf(substring);
+    if (index === -1) {
+        throw new Error(`Not found substring ${substring} in file`);
+    }
+    let version: string = '';
+    index += substring.length; // skip version word
+    while (file[index] !== lastSymbol) {
+        version = version + file[index];
+        index++
+    }
+    return version;
+}
+const readFile  = async (filename: string) => {
+    if (filename){
+        throw new Error('file name is empty')
+    }
+    const file = await fs.promises.readFile(filename, 'utf-8');
     return file;
 }
 
-const upVersion = (version: string[]): string => {
+const upVersion = (version: string[]): string[] => {
     let i = 0;
     if(core.getInput('updType') === 'patch'){
         i = 2;
@@ -43,6 +64,10 @@ const upVersion = (version: string[]): string => {
     let value = Number(version[i]);
     value++;
     version[i] = value.toString();
+    return version;
+   
+}
+const castArrayVersionToString = (version: string[]): string => {
     let retVal = ''
     for (let index = 0; index < version.length; index++) {
        retVal = retVal + version[index];
@@ -52,7 +77,7 @@ const upVersion = (version: string[]): string => {
     }
     return retVal;
 }
-const getVersion = (version: string): string[] => {
+const castStringVersionToArray = (version: string): string[] => {
     let ver: string[] = [];
     let nextPos = 0;
     while (version.indexOf('.', nextPos) !== -1) {
