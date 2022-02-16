fb786f39dff3be91d6f5eaf7a4957101d192fbf9
Ali Musaev <musaev.ae@modul.tech>
2022-02-16 05:24:20 +0300

    a

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,13 @@ import fs from 'fs';
 const main = async () => {
     try {
         const file = await readFile();
-        let index = 2
+        let index = file.indexOf('<Version>');
         console.log('index ' + index)
-        // let version :string = 'v';
-        // index += 9; // skip version word
-        // while (file[index] !== '<') {
-        //     version = version + file[index];
-        // }
+        let version :string = 'v';
+        index += 9; // skip version word
+        while (file[index] !== '<') {
+            version = version + file[index];
+        }
         core.setOutput('version', 2);
     } catch (err: any) {
         core.setFailed(err.message)
